2fc3456acf5ac8f1bff512c88f2229b2ababc8e3
ydcmg <126963900+ydcmg@users.noreply.github.com>
2023-12-04 21:04:24 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -8,7 +8,13 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
 
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
 
+./scripts/feeds update -a
+./scripts/feeds install -a
